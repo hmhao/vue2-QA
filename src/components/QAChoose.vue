3e382432faf87b00fe0a07abb4ae71d5f5483f84
hmh <215795583@qq.com>
2017-05-17 20:17:21 +0800

    add qa game page

---
+++ src/components/QAChoose.vue
@@ -0,0 +1,50 @@
+<template>
+  <div :class="[choose]" v-text="text"></div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+
+export default {
+  name: 'qa-choose',
+  props: ['index', 'text'],
+  computed: {
+    choose () {
+      let c = 'choose-';
+      switch(this.index){
+        case 0: c += 'a';break;
+        case 1: c += 'b';break;
+        case 2: c += 'c';break;
+        case 3: c += 'd';break;
+      }
+      return c
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.choose-size {
+  width: 336px;
+  height: 72px;
+  margin-bottom: 20px;
+}
+
+.choose-a {
+  background: url(../assets/images/choose_a.png) no-repeat;
+  .choose-size();
+}
+.choose-b {
+  background: url(../assets/images/choose_b.png) no-repeat;
+  .choose-size();
+}
+.choose-c {
+  background: url(../assets/images/choose_c.png) no-repeat;
+  .choose-size();
+}
+.choose-d {
+  background: url(../assets/images/choose_d.png) no-repeat;
+  .choose-size();
+}
+
+</style>
--- src/page/Game.vue
+++ src/page/Game.vue
@@ -1,17 +1,45 @@
 <template>
   <div class="item-page" :class="{animate: active}">
-    <qa-item></qa-item>
+    <div class="score-bar">
+      <span class="score">{{score}}/{{total}}</span>
+      <img class="bar" src="../assets/images/score_bar.png">
+      <div class="progress" :style="{width: score + 'px'}"></div>
+    </div>
+    <div class="question">
+      <span class="title">第{{index}}题</span>
+      <div class="content" v-text="content"></div>
+    </div>
+    <div class="chooses">
+      <qa-choose v-for="(choose, i) in chooses" :key="choose.id"
+        :index="i"
+        :text="choose"></qa-choose>
+    </div>
+    <img class="bottom" src="../assets/images/game_page_bottom_star.png">
   </div>
 </template>
 
 <script>
-import qaItem from '@/components/QAItem'
+import qaChoose from '@/components/QAChoose'
 
 export default {
 	name: 'game',
+  data () {
+    return {
+      score: 40,
+      total: 120,
+      index: 1,
+      content: '我是问题，我是问题，我是问题，我是问题，我是问题？',
+      chooses: [
+        'a',
+        'b',
+        'c',
+        'd'
+      ]
+    }
+  },
   props: ['active'],
 	components: {
- 		qaItem
+ 		qaChoose
 	},
 	created(){
 		//this.$store.commit('REMBER_TIME');
@@ -28,5 +56,66 @@ export default {
   width: 100%;
   height: 100%;
   text-align: center;
+
+  .score-bar {
+    position: relative;
+    top: 20px;
+    width: 390px;
+    height: 65px;
+    margin:0 auto;
+    transform: scale(.8);
+
+    .score {
+      position: absolute;
+      top: -15px;
+      left: 170px;
+      color: #fbf05e;
+    }
+
+    .bar {
+      position: absolute;
+      left: 0;
+    }
+
+    .progress {
+      position: absolute;
+      height: 30px;
+      left: 45px;
+      top: 26px;
+      background: url(../assets/images/score_progress.png) no-repeat;
+      -webkit-mask-image: url(../assets/images/score_progress_mask.jpg);
+    }
+  }
+
+  .question {
+    background: url(../assets/images/question_box.png) no-repeat;
+    width: 420px;
+    height: 174px;
+    margin: 40px auto 0;
+    transform: scale(.9);
+
+    .title {
+      margin: 5px 0 0;
+    }
+
+    .content {
+      margin-top: 24px;
+      padding: 5px 10px;
+    }
+  }
+
+  .chooses {
+    width: 340px;
+    margin: 0 auto;
+    transform: scale(.9);
+  }
+
+  .bottom {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+  }
 }
+
 </style>
